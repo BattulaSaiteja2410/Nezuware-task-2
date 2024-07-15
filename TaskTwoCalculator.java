d09217508ec3c64d84fdfb454df451b827bd5c0a
BattulaSaiteja2410 <138872218+BattulaSaiteja2410@users.noreply.github.com>
2024-07-15 23:40:51 +0530

    Add files via upload

---
+++ TaskTwoCalculator.java
@@ -0,0 +1,61 @@
+/*Creating a Calculator
+Developing a Java console-based calculator application capable of performing basic
+arithmetic operations such as addition, subtraction, multiplication, and
+division. Users input two numbers and select an operation, and the calculator
+displays the result. The application should handle invalid inputs and division by
+zero gracefully.
+ */
+import java.lang.annotation.AnnotationTypeMismatchException;
+import java.util.Scanner;
+
+public class TaskTwoCalculator {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		System.out.println("enter the 1st value");
+		int n1=sc.nextInt();
+		System.out.println("Please enter arithmetic opporetor Symbal");
+		String symbal;
+		while(true) {
+			try {
+			symbal=sc.next();
+		switch(symbal){
+			case"+":
+				System.out.println("enter the 2nd value");
+				int n2=sc.nextInt();
+				int res1=n1+n2;
+			System.out.println("result :"+n1+"+"+n2+"="+res1);
+			break;
+			case"-":
+				System.out.println("enter the 2nd value");
+				int n3=sc.nextInt();
+				int res2=n1-n3;
+			System.out.println("result :"+n1+"-"+n3+"="+res2);
+			break;
+			case"*":
+				System.out.println("enter the 2nd value");
+				int n4=sc.nextInt();
+				int res3=n1*n4;
+			System.out.println("result :"+n1+"*"+n4+"="+res3);	
+			break;
+			case"/":System.out.println("enter the 2nd value");
+			int n5=sc.nextInt();
+			if (n5!=0) {
+				int res4=n1/n5;
+			System.out.println("result :"+n1+"/"+n5+"="+res4);
+			break;
+			}
+			else {
+				System.out.println("We can not perform the oparation like any-number/zero");
+			}
+			default:
+				System.err.println("please enter correct Symbal");
+		}
+			}
+		catch (AnnotationTypeMismatchException ea) {
+			sc.next();
+		}
+		}
+	}
+}
+
+	
